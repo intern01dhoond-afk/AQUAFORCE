4b8497dfaa99290ca09d1294e239fc70d515cc79
itshemanthcode <intern01.dhoond@gmail.com>
2026-08-25 10:06:35 +0530

    Enable unoptimized images for direct static loading

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,7 @@ const nextConfig = {
   basePath: "/aquaforceforautocare",
   devIndicators: false,
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 2880, 3840],
-    imageSizes: [64, 128, 256, 384, 512, 640, 768, 1024],
-    qualities: [75, 80, 85, 90, 95, 100],
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
