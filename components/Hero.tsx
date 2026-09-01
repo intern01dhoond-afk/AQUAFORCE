06885132238e00785547747e35ff2cc8306dc9aa
itshemanthcode <intern01.dhoond@gmail.com>
2026-09-01 13:18:49 +0530

    1st Sept update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -490,7 +490,7 @@ export default function Hero() {
         {/* Mobile Background Image */}
         <div className="sm:hidden absolute inset-0 z-0">
           <Image
-            src="/aquaforceforautocare/images/Hero%20section%20Images/4.2.png"
+            src="/aquaforceforautocare/images/Hero%20section%20Images/4.2.webp"
             alt="Aquaforce 1400 Cordless Powerful Portable Bike Washing"
             fill
             priority
@@ -503,7 +503,7 @@ export default function Hero() {
         {/* Desktop Background Image */}
         <div className="hidden sm:block absolute inset-0 z-0">
           <Image
-            src="/aquaforceforautocare/images/Hero%20section%20Images/4.webp"
+            src="/aquaforceforautocare/images/Hero%20section%20Images/4.2.webp"
             alt="Aquaforce 1400 Cordless Powerful Portable Adventure Bike Washing"
             fill
             priority
